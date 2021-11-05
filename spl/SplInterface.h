61f1c4518c8ac5dc502b63279e2d791cbafd1722
Ricardo Lu <shenglu1202@163.com>
2021-11-05 17:24:19 +0800

    refactor<SplInterface.h>: replace SplCallbacks structure to class.

--- spl/SplInterface.h
+++ spl/SplInterface.h
@@ -4,7 +4,7 @@
  * @Author: Ricardo Lu<shenglu1202@163.com>
  * @Date: 2021-10-27 11:07:52
  * @LastEditors: Ricardo Lu
- * @LastEditTime: 2021-10-28 12:01:05
+ * @LastEditTime: 2021-11-05 17:18:50
  */
 
 #ifndef __TS_SPL_INTERFACE_H__
@@ -12,12 +12,29 @@
 
 #include "Common.h"
 
-typedef struct {
-    TsPutDataFunc    putData    { NULL }; // args[0]
-    TsGetResultFunc  getResult  { NULL }; // args[1]
-    TsProcResultFunc procResult { NULL }; // args[2]
-    gpointer         args[3]    { NULL, NULL, NULL };
-} SplCallbacks;
+class SplCallbacks{
+public:
+    SplCallbacks (
+        TsPutDataFunc    cbPutData,
+        TsGetResultFunc  cbGetResult,
+        TsProcResultFunc cbProcResult,
+        gpointer         arg0,
+        gpointer         arg1,
+        gpointer         arg2) {
+        putData    = cbPutData   ;
+        getResult  = cbGetResult ;
+        procResult = cbProcResult;
+        args [0]   = arg0;
+        args [1]   = arg1;
+        args [2]   = arg2;
+    }
+
+public:
+    TsPutDataFunc    putData;    //args[0]
+    TsGetResultFunc  getResult;  //args[1]
+    TsProcResultFunc procResult; //args[2]
+    gpointer         args[3];
+};
 
 extern "C"  void* splInit (const std::string& args);
 
