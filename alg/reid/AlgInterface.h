4d03fedcb5c418dedb40ba41225427007bd434a0
Ricardo Lu <shenglu1202@163.com>
2021-11-11 14:18:09 +0800

    feat<reid>: add batch support interface.

--- alg/reid/AlgInterface.h
+++ alg/reid/AlgInterface.h
@@ -3,8 +3,8 @@
  * @version: 1.0
  * @Author: Ricardo Lu<shenglu1202@163.com>
  * @Date: 2021-11-01 09:34:02
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-11-04 16:26:24
+ * @LastEditors: Ricardo Lu
+ * @LastEditTime: 2021-11-11 14:16:17
  */
 
 #ifndef __TS_ALG_INTERFACE_H__
@@ -12,19 +12,18 @@
 
 #include "Common.h"
 
-extern "C"  void* algInit (const std::string& args);
-
-extern "C"  bool algStart (void* alg);
-
-extern "C"  std::shared_ptr<TsJsonObject> algProc (
-    void* alg, const std::shared_ptr<TsGstSample>& data);
-
-extern "C"  bool algCtrl (void* alg, const std::string& cmd);
-
-extern "C"  void algStop (void* alg);
-
-extern "C"  void algFina (void* alg);
-
-extern "C"  bool algSetCb (void* alg, TsPutResult cb, void* args);
+extern "C" void* algInit  (const std::string&                        );
+extern "C" void* algInit2 (void*, const std::string&                 );
+extern "C" bool  algStart (void*                                     );
+extern "C" std::shared_ptr<TsJsonObject>
+                 algProc  (void*, const std::shared_ptr<TsGstSample>&);
+extern "C" std::shared_ptr<std::vector<std::shared_ptr<TsJsonObject>>>
+                 algProc2 (void*, const std::shared_ptr<std::vector<
+                           std::shared_ptr<TsGstSample>>>&           );
+extern "C" bool  algCtrl  (void*, const std::string&                 );
+extern "C" void  algStop  (void*                                     );
+extern "C" void  algFina  (void*                                     );
+extern "C" bool  algSetCb (void*, TsPutResult,  void*                );
+extern "C" bool  algSetCb2(void*, TsPutResults, void*                );
 
 #endif //__TS_ALG_INTERFACE_H__
